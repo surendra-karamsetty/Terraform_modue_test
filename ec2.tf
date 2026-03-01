bc4781da036324c838153f3a532eb383185d718e
surendra-karamsetty <surendra3207@gmail.com>
2026-03-01 18:52:52 +0530

    Commit Changes

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 module "ec2" {
     #source = "../terraform_module"
     source = "git::https://github.com/surendra-karamsetty/terraform_module.git?ref=main"
-    project = "Roboshop"
-    environment = "dev"
-    ami_id = "ami-0220d79f3f480ecf5"
-    sg_ids = ["sg-0b9f1fbcf18f4096e"]
+    project = "${var.project}-${var.environment}"
+    environment = var.environment
+    ami_id = var.ami_id
+    sg_ids = var.sg_ids
     instance_type = "t3.micro"
 }
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+    default =  "ami-0220d79f3f480ecf5"
+}
+
+variable "sg_ids" {
+    default = ["sg-0b9f1fbcf18f4096e"]
+}
+
+variable "project" {
+    default = "Roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
