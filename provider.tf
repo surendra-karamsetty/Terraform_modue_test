6e2c28b5c37769cfe44af103546c0ff6a5011ef4
surendra-karamsetty <surendra3207@gmail.com>
2026-03-01 18:56:14 +0530

    Commit Changes

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 
    backend "s3" {
     bucket         = "venkata-state-aws-88s"
-    key            = "for_each-state.tfstate"
+    key            = "module-state.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile = true
